dc78eb7dd3a013d35fc8a780775471fa3470805d
Anjini Katari <akatari30@stuy.edu>
2022-11-28 13:18:31 -0500

    classwork

--- 23_fd_AdorableDidacticElephants/app.py
+++ 23_fd_AdorableDidacticElephants/app.py
@@ -4,6 +4,7 @@
 #2022-11-21
 #time spent: <elapsed time in hours, rounded to nearest tenth>
 
+
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -12,22 +13,8 @@ app = Flask(__name__)
 
 @app.route("/")       
 def hello_world():
-    file = open("key_nasa.txt", 'r')
-    file = file.read()
-    print(file)
-
-    x = requests.get('https://api.nasa.gov/planetary/apod?api_key=' + file)
-    print(x)
-    print(x.request)
-    print(x.json())
-    #print(x.content)
-    json = x.json()
-    image_url = json['url']
-    explanation = json["explanation"]
-    title = json["title"]
-    print(image_url)
-
-    return render_template("main.html", image = image_url, explanation = explanation, title = title)
+    
+    return render_template("news-magazine.html")
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
